3a06dd6ed47a9a8f7d2a470677aaa06690f44496
Márvio André Bezerra Silverio <marvio.bezerra@hotmail.com>
2018-03-02 02:06:24 -0500

    Added Snake and Zombie01 animations

---
+++ Assets/Scripts/ZombieController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour {
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            anim.SetBool("Walk", true);
+            anim.SetBool("Atack", false);
+            return;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            anim.SetBool("Walk", false);
+            anim.SetBool("Atack", true);
+            return;
+        }
+
+        if (Input.GetKey("s"))
+        {
+            anim.SetBool("Walk", false);
+            anim.SetBool("Atack", false);
+        }
+        
+    }
+}
