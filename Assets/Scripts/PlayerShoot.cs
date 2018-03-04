c2c6326b7b9154afece3d31fdb4c886a4d48cce1
Renato Luiz Carneiro Junior <rejr8@hotmail.com>
2018-03-03 23:19:02 -0500

    Including controls and killing zombies

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject bullet;
+
+    private void OnMouseDown()
+    {
+        player.GetComponent<Animator>().SetBool("Shot", true);
+        Transform hip = player.transform.Find("Bones/Hip");
+
+        var bulletObject = Instantiate(bullet, hip.transform.position, Quaternion.identity);
+        bulletObject.GetComponent<Rigidbody2D>().AddForce((transform.position - player.transform.position) * 5, ForceMode2D.Impulse);
+        Destroy(bulletObject, 1.0f);
+    }
+
+    private void OnMouseUp()
+    {
+        player.GetComponent<Animator>().SetBool("Shot", false);
+    }
+}
--- Assets/Scripts/Player_Control.cs
+++ Assets/Scripts/Player_Control.cs
@@ -3,48 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Control : MonoBehaviour {
-
     public float maxSpeed = 10.0f;
   
-    private SpriteRenderer sRend;
     private Rigidbody2D rBody;
+    private Vector3 mousePosition;
 
     // Use this for initialization
     void Start () {
         rBody = GetComponent<Rigidbody2D>();
-        sRend = GetComponent<SpriteRenderer>();
-        
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
     void FixedUpdate(){
+        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+
+        //transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        Transform hip = transform.Find("Bones/Hip");
+        hip.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+
         float moveHoriz = Input.GetAxis("Horizontal");
         float moveVert = Input.GetAxis("Vertical");
 
-        
-
-        //animator.SetFloat("speed", Mathf.Abs(moveVert));
-
-        //Debug.Log("Horizontal " + moveHoriz + "Vertical" + moveVert);
         rBody.velocity = new Vector2(moveHoriz * maxSpeed, rBody.velocity.y);
-       
         rBody.velocity = new Vector2(rBody.velocity.x, moveVert * maxSpeed);
-
-        if (moveHoriz > 0 )
-        {
-            sRend.flipX = false;
-
-        }
-        else if (moveHoriz < 0 )
-        {
-            sRend.flipX = true;
-        }
-
-
-
-
     }
- }
+
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return (Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg) + 180;
+    }
+}
