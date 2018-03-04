342d78a0e897246a3f298d4ce4f121a4e6dc745f
Manoel Britto <manoelbritto@gmail.com>
2018-03-03 19:27:44 -0500

    include Enemy script

---
+++ Assets/Scripts/Player_Control.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Control : MonoBehaviour {
+
+    public float maxSpeed = 10.0f;
+  
+    private SpriteRenderer sRend;
+    private Rigidbody2D rBody;
+
+    // Use this for initialization
+    void Start () {
+        rBody = GetComponent<Rigidbody2D>();
+        sRend = GetComponent<SpriteRenderer>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void FixedUpdate(){
+        float moveHoriz = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+
+        
+
+        //animator.SetFloat("speed", Mathf.Abs(moveVert));
+
+        //Debug.Log("Horizontal " + moveHoriz + "Vertical" + moveVert);
+        rBody.velocity = new Vector2(moveHoriz * maxSpeed, rBody.velocity.y);
+       
+        rBody.velocity = new Vector2(rBody.velocity.x, moveVert * maxSpeed);
+
+        if (moveHoriz > 0 )
+        {
+            sRend.flipX = false;
+
+        }
+        else if (moveHoriz < 0 )
+        {
+            sRend.flipX = true;
+        }
+
+
+
+
+    }
+ }
