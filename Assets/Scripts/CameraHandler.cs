93c07ce7a0332b889b0d3f3260ac02b467361974
Renato Luiz Carneiro Junior <rejr8@hotmail.com>
2018-03-06 02:03:49 -0500

    Created limits to camera script

--- Assets/Scripts/CameraHandler.cs
+++ Assets/Scripts/CameraHandler.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class CameraHandler : MonoBehaviour
 {
     private Transform player;
+
+    public Vector2 XLimits = new Vector2(3, 3);
+    public Vector2 YLimits = new Vector2(3, 3);
     public Vector2 smoothing = new Vector2(3, 3);
     
     void Awake()
@@ -16,9 +19,26 @@ public class CameraHandler : MonoBehaviour
     {
         var x = transform.position.x;
         var y = transform.position.y;
-        
+
         x = Mathf.Lerp(x, player.position.x, smoothing.x * Time.deltaTime);
         y = Mathf.Lerp(y, player.position.y, smoothing.y * Time.deltaTime);
+
+        if (x <= XLimits.x)
+        {
+            x = XLimits.x;
+        }else if(x >= XLimits.y)
+        {
+            x = XLimits.y;
+        }
+        
+        if (y <= YLimits.x)
+        {
+            y = YLimits.x;
+        }
+        else if (y >= YLimits.y)
+        {
+            y = YLimits.y;
+        }
 
         transform.position = new Vector3(x, y, transform.position.z);
     }
