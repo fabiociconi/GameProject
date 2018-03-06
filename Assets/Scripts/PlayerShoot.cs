e45a7cd65b7a5b82968b429b06def6c1ce924fce
Renato Luiz Carneiro Junior <rejr8@hotmail.com>
2018-03-05 23:57:26 -0500

    Adjusting player prefab and shot

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -9,31 +9,42 @@ public class PlayerShoot : MonoBehaviour {
 
     private bool rightGun = false;
 
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Transform hip;
+
+            if (rightGun)
+            {
+                hip = player.transform.Find("Bones/Hip/RightArmIk");
+            }
+            else
+            {
+                hip = player.transform.Find("Bones/Hip/LeftArmIk");
+            }
+
+            rightGun = !rightGun;
+
+            player.GetComponent<Animator>().SetBool("Shot", true);
+
+
+            var bulletObject = Instantiate(bullet, hip.transform.position, Quaternion.identity);
+            bulletObject.GetComponent<Rigidbody2D>().AddForce((transform.position - player.transform.position) * 5, ForceMode2D.Impulse);
+            Destroy(bulletObject, 1.0f);
+        }else if (Input.GetMouseButtonUp(0))
+        {
+            player.GetComponent<Animator>().SetBool("Shot", false);
+        }
+    }
+
     private void OnMouseDown()
     {
-        Transform hip;
-
-        if (rightGun)
-        {
-            hip = player.transform.Find("Bones/Hip/RightArmIk");
-        }
-        else
-        {
-            hip = player.transform.Find("Bones/Hip/LeftArmIk");
-        }
-
-        rightGun = !rightGun;
-
-        player.GetComponent<Animator>().SetBool("Shot", true);
         
-
-        var bulletObject = Instantiate(bullet, hip.transform.position, Quaternion.identity);
-        bulletObject.GetComponent<Rigidbody2D>().AddForce((transform.position - player.transform.position) * 5, ForceMode2D.Impulse);
-        Destroy(bulletObject, 1.0f);
     }
 
     private void OnMouseUp()
     {
-        player.GetComponent<Animator>().SetBool("Shot", false);
+        
     }
 }
