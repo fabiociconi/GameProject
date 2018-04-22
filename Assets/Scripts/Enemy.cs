b8828c09393563e84715b4709c008fddcbed7bc8
Renato Luiz Carneiro Junior <rejr8@hotmail.com>
2018-04-22 02:42:57 -0400

    Adjusting enemy movements

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,93 +3,84 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float attackRange;
+    public float followRange = 40;
+    public float attackRange = 5;
+    public enum State { Idle, Following, Attacking};
 
-
-    public enum State { Idle, Stopping, Attacking };
     private Animator animator;
-
-
     private Transform target;
-    private State currentState;
+    private State currentState = State.Idle;
+    private float distanceToPlayer;
+    private Rigidbody2D rBody;
 
     void Start()
     {
         target = GameObject.FindGameObjectsWithTag("Player")[0].transform;
         currentState = State.Idle;
         animator = GetComponent<Animator>();
-
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag.Equals("Environment"))
-        {
-            //Destroy(this.gameObject);
-            //currentState = State.Stopping;
-            //animator.SetBool("Walk", false);
-            //animator.SetBool("Atack", false);
-        }
-
-        if (collision.gameObject.tag.Equals("Player"))
-        {
-            currentState = State.Attacking;
-            animator.SetBool("Walk", false);
-            animator.SetBool("Atack", true);
-        }
+        rBody = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
-        float distanceToPlayer = Vector3.Distance(transform.position, target.position);
-        if (distanceToPlayer < attackRange)
+        distanceToPlayer = Vector3.Distance(transform.position, target.position);
+
+        if (rBody.velocity.x > 0 || rBody.velocity.y > 0)
         {
-            Vector3 targetDir = target.position - transform.position;
-            float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg - 90f;
-
-            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 90 * Time.deltaTime);
-
-            StartCoroutine(Attack());
-
-
-
+            animator.SetBool("Walk", true);
         }
         else
         {
-            currentState = State.Idle;
             animator.SetBool("Walk", false);
-            animator.SetBool("Atack", false);
         }
 
+        switch (currentState)
+        {
+            case State.Idle:
+                Idle();
+                break;
 
+            case State.Following:
+                Follow();
+                break;
+
+            case State.Attacking:
+                Attack();
+                break;
+        }
     }
 
-    //move the zombie to attack the player
-    IEnumerator Attack()
+    void Idle()
     {
-        float attackSpeed = 0.4f;
-        float percent = 0;
-
-
-        Vector3 originalPosition = transform.position;
-        Vector3 dirToTarget = (target.position - transform.position).normalized;
-        Vector3 attackPosition = target.position - dirToTarget * 0.5f;
-
-        while (percent <= 0.8 && currentState == State.Idle)
+        if (distanceToPlayer < followRange)
         {
-            percent += Time.deltaTime * attackSpeed;
-            float interpolation = percent;
-            transform.position = Vector3.Lerp(originalPosition, attackPosition, interpolation);
-            animator.SetBool("Walk", true);
-            animator.SetBool("Atack", false);
+            currentState = State.Following;
+        }
+    }
 
-            yield return null;
+    private void Follow()
+    {
+        Vector3 targetDir = target.position - transform.position;
+        float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg - 90f;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 90 * Time.deltaTime);
+        
+        rBody.velocity = target.transform.position - transform.position;
+    }
+
+    private void Attack()
+    {
+        if (distanceToPlayer > attackRange)
+        {
+            currentState = State.Following;
+            animator.SetBool("Atack", false);
         }
 
+        animator.SetBool("Atack", true);
+    }
 
-
-        //currentState = State.Attacking;
-
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        currentState = State.Attacking;
     }
 }
